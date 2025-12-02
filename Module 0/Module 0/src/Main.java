09f55c8beab8f7ec5d7cb747bdfb3c7e7cd3345b
Yervant-Kairuz <yervantanthony.kairuz@benilde.edu.ph>
2025-12-02 15:09:18 +0800

    Added Module 0 Project

---
+++ Module 0/Module 0/src/Main.java
@@ -0,0 +1,8 @@
+void main() {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("What is your name?");
+    String name = scanner.nextLine();
+    System.out.println("Hello " + name);
+
+}
