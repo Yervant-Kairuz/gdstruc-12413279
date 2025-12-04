467a7b6003066ef26a193e6dfe13c691ea934338
Yervant-Kairuz <yervantanthony.kairuz@benilde.edu.ph>
2025-12-04 17:46:36 +0800

    Quiz 4 | Status: Finished

---
+++ Module 5/Module 5 - Hashtables/src/Main.java
@@ -0,0 +1,21 @@
+void main() {
+    Player ploo = new Player(134, "Plooful", 135);
+    Player wardell = new Player(536, "TSM Wardell", 648);
+    Player deadlyJimmy = new Player(32, "DeadlyJimmy", 34);
+    Player subroza = new Player(4931, "Subroza", 684);
+    Player annieDro = new Player(6919, "C9 Annie", 593);
+
+    SimpleHashtable hashtable = new SimpleHashtable();
+    hashtable.put(ploo.getUserName(),  ploo);
+    hashtable.put(wardell.getUserName(), wardell);
+    hashtable.put(deadlyJimmy.getUserName(), deadlyJimmy);
+    hashtable.put(subroza.getUserName(), subroza);
+    hashtable.put(annieDro.getUserName(), annieDro);
+
+    hashtable.printHashtable();
+    System.out.println("── Getting Subroza ──");
+    System.out.println(hashtable.get("Subroza"));
+    System.out.println("── Removed Subroza ──");
+    hashtable.remove("Subroza");
+    hashtable.printHashtable();
+}
